45907ef62e16546202aacf10a509483bad4abea7
Ahn Sieun <83575791+NopeSieun@users.noreply.github.com>
2022-07-24 01:36:12 +0900

    [Silver IV] Title: 한수, Time: 72 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1065. 한수/한수.py
@@ -0,0 +1,10 @@
+num = int(input())
+
+h = 0
+for i in range(1, num+1):
+    n = list(map(int,str(i)))
+    if i < 100:
+        h += 1
+    elif n[0] - n[1] == n[1] - n[2]:
+        h += 1
+print(h)
